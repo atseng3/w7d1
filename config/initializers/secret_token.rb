6017aabd535821fba858c51dcd2097eaac20546e
= <student@appacademy.io>
2013-10-28 11:23:42 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoTagger::Application.config.secret_token = '4d9648b183444cdfe71a79d33f87daa66b262154fa2330dea8d59654bfe593dcc43d1a45418db4b320df77a66c563f3d7f80fb2459f2709c672b423a0923628e'
